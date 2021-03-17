e2247aa0a64940ec68c50541f91d6e1c1bdebada
Alex Gada <alexgada01@gmail.com>
2021-03-17 19:44:01 +0000

    heroku

--- dbConnection.js
+++ dbConnection.js
@@ -1,3 +1,21 @@
+// const ENV = process.env.NODE_ENV || "development";
+// const knex = require("knex");
+
+// const dbConfig =
+//   ENV === "production"
+//     ? {
+//         client: "pg",
+//         connection: {
+//           connectionString: process.env.DATABASE_URL,
+//           ssl: {
+//             rejectUnauthorized: false,
+//           },
+//         },
+//       }
+//     : require("../knexfile");
+
+// module.exports = knex(dbConfig);
+
 const knex = require("knex");
 const dbConfig = require("./knexfile");
 const ENV = process.env.NODE_ENV || "development";
