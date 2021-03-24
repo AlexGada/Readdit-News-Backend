3bbbd8feafe5cc9b5da2457228aa6ba7c03191ea
Alex Gada <alexgada01@gmail.com>
2021-03-24 20:38:39 +0000

    updated readme

--- dbConnection.js
+++ dbConnection.js
@@ -16,6 +16,7 @@ const dbConfig =
 
 module.exports = knex(dbConfig);
 
+// testing connection settings
 // const knex = require("knex");
 // const dbConfig = require("./knexfile");
 // const ENV = process.env.NODE_ENV || "development";
